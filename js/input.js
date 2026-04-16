1351d1364b494c2d7490ef6600cd024e4e930db7
Andrea <aschneeweiss@centro.edu.mx>
2026-04-16 17:01:32 -0600

    Add input form with dynamic title and color change functionality

---
+++ js/input.js
@@ -0,0 +1,16 @@
+let title = document.querySelector("h1");
+let inputText = document.getElementById("input-text");
+let inputColor = document.getElementById("input-color");
+let inputBackgroundColor = document.getElementById("input-background-color");
+inputText.addEventListener("input", function() {
+    const value = inputText.value;
+    title.textContent = value;
+})
+inputColor.addEventListener("input", function() {
+    const value = inputColor.value;
+    title.style.color = value;  
+})
+inputBackgroundColor.addEventListener("input", function() {
+    const value = inputBackgroundColor.value;
+    document.body.style.backgroundColor = value;
+})
